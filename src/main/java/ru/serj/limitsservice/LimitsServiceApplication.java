29850cbc8150fa6e3dd86238a7c8064e6363cd91
serg <serj.teplov@gmail.com>
2020-01-13 07:47:51 +0300

    yet another commit

--- src/main/java/ru/serj/limitsservice/LimitsServiceApplication.java
+++ src/main/java/ru/serj/limitsservice/LimitsServiceApplication.java
@@ -3,9 +3,11 @@ package ru.serj.limitsservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableConfigurationProperties(ru.serj.limitsservice.Configuration.class)
+@EnableDiscoveryClient
 public class  LimitsServiceApplication {
 
     public static void main(String[] args) {
