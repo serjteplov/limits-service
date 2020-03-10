fe3025d078c510cb3bf578a0e3aeffd855c12a79
serg <serj.teplov@gmail.com>
2020-03-10 16:11:36 +0300

    hystrix fault tolerance

--- src/main/java/ru/serj/limitsservice/LimitsConfigurationController.java
+++ src/main/java/ru/serj/limitsservice/LimitsConfigurationController.java
@@ -1,5 +1,6 @@
 package ru.serj.limitsservice;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +14,16 @@ public class LimitsConfigurationController {
 
     @GetMapping("/limits")
     public LimitConfiguration retrieveLimitsFromConfigurations() {
-        return new LimitConfiguration(configuration.getMaximum(),configuration.getMinimum());
+        return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
     }
 
+    @GetMapping("/fault-tolerance-example")
+    @HystrixCommand(fallbackMethod = "fallbackRetrieveConfiguration")
+    public LimitConfiguration retrieveConfiguration() {
+        throw new RuntimeException("Not available");
+    }
+
+    public LimitConfiguration fallbackRetrieveConfiguration() {
+        return new LimitConfiguration(9999, 9);
+    }
 }
--- src/main/java/ru/serj/limitsservice/LimitsServiceApplication.java
+++ src/main/java/ru/serj/limitsservice/LimitsServiceApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableConfigurationProperties(ru.serj.limitsservice.Configuration.class)
 @EnableDiscoveryClient
+@EnableHystrix
 public class  LimitsServiceApplication {
 
     public static void main(String[] args) {
